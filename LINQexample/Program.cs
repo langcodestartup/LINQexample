11a78ffc329a1566bf264fe88496d04bea79ea2f
김민준 <admin@xflow.kr>
2019-10-01 15:06:50 +0900

    초기 커밋.

---
+++ LINQexample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
